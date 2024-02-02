1ff17423fe38ed856479823dfe4df258640b9dba
Tonmuy Ahamed Sifat <tonmoysifatmd@gmail.com>
2024-02-03 01:39:59 +0600

    Final_commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
-const port = process.env.PORT || 9001;
-// const port = 9001;
+// const port = process.env.PORT || 9001;
+const port = 9001;
 
 app.listen(port, () => {
   console.log(`server is running port http://localhost:${port}`);
